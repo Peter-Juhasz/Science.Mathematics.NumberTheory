70f912971966fff60d7743612b4c36c31e3ba372
Péter Juhász <peter.juhasz@live.com>
2024-06-19 13:44:32 +0200

    euler's lucky numbers

---
+++ src/Science.Mathematics.NumberTheory/IntegerExtensions.EulersLuckyNumber.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+
+namespace Science.Mathematics.NumberTheory;
+
+public static partial class IntegerExtensions
+{
+    /// <summary>
+    /// Checks whether a number is an Euler's lucky number.
+    /// 
+    /// See also:
+    /// https://en.wikipedia.org/wiki/Lucky_numbers_of_Euler
+    /// https://mathworld.wolfram.com/LuckyNumberofEuler.html
+    /// https://oeis.org/A014556
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static bool IsEulersLuckyNumber<T>(this T n) where T :
+        INumberBase<T>, IBinaryInteger<T>,
+        IComparisonOperators<T, T, bool>, IModulusOperators<T, T, T>
+    {
+        if (n.IsNegative())
+        {
+            return false;
+        }
+
+        for (T i = T.Zero; i < n; i++)
+        {
+            T result = i * i - i + n;
+            if (!result.IsPrime())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static bool IsCabooseNumber<T>(this T n) where T :
+        INumberBase<T>, IBinaryInteger<T>,
+        IComparisonOperators<T, T, bool>, IModulusOperators<T, T, T> =>
+        n.IsEulersLuckyNumber();
+}
---
+++ test/Science.Mathematics.NumberTheory.Tests/EulerLuckyNumberTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Science.Mathematics.NumberTheory.Tests;
+
+[TestClass]
+public class EulerLuckyNumberTests
+{
+    [TestMethod]
+    public void Is_41()
+    {
+        Assert.IsTrue(41.IsEulersLuckyNumber());
+    }
+}
